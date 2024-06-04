158b5f58780ff83676a12606290c1ca31a0f5d1a
Souleymane <150073079+Souley20@users.noreply.github.com>
2024-06-04 13:51:34 +0200

    Update DashboardControllerEmploye.php

--- src/Controller/Admin/DashboardControllerEmploye.php
+++ src/Controller/Admin/DashboardControllerEmploye.php
@@ -40,7 +40,7 @@ class DashboardControllerEmploye extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Ecf Garage Automobile');
+            ->setTitle('Garage Automobile');
     }
 
     public function configureMenuItems(): iterable
